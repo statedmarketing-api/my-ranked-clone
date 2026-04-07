7f19e34ad34689cf77377c0ad425d03dfcfdb696
statedmarketing-api <statedmarketing@gmail.com>
2026-04-07 15:47:12 -0700

    Create tailwind.config.js

---
+++ frontend/tailwind.config.js
@@ -0,0 +1,1 @@
+js\n/** @type {import('tailwindcss').Config} */\nmodule.exports = {\n content: ['./app/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],\n theme: { extend: {} },\n plugins: [],\n};\n
